b7e969d904c5e15657068b40a2005ba6a2095a7a
Dorcsinecz Gabor <gabor.dorcsinecz@digital.hmrc.gov.uk>
2019-08-28 15:39:08 +0100

    Refactor

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,40 +1,33 @@
 package tutorial.webapp
 
-
 import org.scalajs.dom
 import dom.document
-
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 
 object TutorialApp {
   def main(args: Array[String]): Unit = {
-    addParagraph("Hello Word")
-  }
-
-  def addParagraph(in: String): Unit = {
-    document.body.appendChild(document.createElement("p").appendChild(document.createTextNode(in)))
+    addParagraph(document.body, "Hello Word")
     addButton()
   }
 
   @JSExportTopLevel("addClickedMessage")
   def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
+    addParagraph(document.body, "You clicked the button!")
   }
-
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
-  }
-
 
   def addButton() = {
     val button = document.createElement("button")
     button.innerHTML= "Click Me!"
     button.setAttribute("onClick", "addClickedMessage()")
     document.body.appendChild(button)
+  }
+
+  def addParagraph(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
   }
 
 }
