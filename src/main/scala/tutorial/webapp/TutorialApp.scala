4284c2ad3203fec1a2fb8bd1a8e3faa610fc990f
Dorcsinecz Gabor <gabor.dorcsinecz@digital.hmrc.gov.uk>
2019-08-29 13:35:43 +0100

    Added 2D Context to draw on

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -2,13 +2,17 @@ package tutorial.webapp
 
 import org.scalajs.dom
 import dom.document
+import org.scalajs.dom.html.Canvas
+
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 
 object TutorialApp {
   def main(args: Array[String]): Unit = {
-    addParagraph(document.body, "Hello Word")
-    addButton()
+
+    addParagraph(createParagraph(), "Hello Word")
+    addButton(createParagraph())
+    initScreen(createParagraph())
   }
 
   @JSExportTopLevel("addClickedMessage")
@@ -16,18 +20,40 @@ object TutorialApp {
     addParagraph(document.body, "You clicked the button!")
   }
 
-  def addButton() = {
+  def createParagraph():dom.Node = {
+    val p = document.createElement("p")
+    document.body.appendChild(p)
+    p
+  }
+
+  def addButton(targetNode:dom.Node) = {
     val button = document.createElement("button")
     button.innerHTML= "Click Me!"
     button.setAttribute("onClick", "addClickedMessage()")
-    document.body.appendChild(button)
+    targetNode.appendChild(button)
+    button
   }
 
-  def addParagraph(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
+  def addParagraph(targetNode: dom.Node, text: String): dom.Node = {
     val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+    targetNode.appendChild(textNode)
+  }
+
+
+  def initScreen(targetNode:dom.Node): dom.CanvasRenderingContext2D = {
+    val width = 600
+    val height = 400
+    val canvas = document.createElement("canvas").asInstanceOf[Canvas]
+    canvas.width = width
+    canvas.height = height
+
+    val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
+    ctx.fillStyle = "#40E0D0"
+    ctx.fillRect(0,0,width,height)
+
+    document.createElement("p")
+    targetNode.appendChild(canvas)
+    ctx
   }
 
 }
